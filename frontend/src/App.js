99e8e25b8dbfae76487528d599fccb976634837a
Daniel Viana <vianadaniel@hotmail.com>
2020-12-30 16:41:52 -0300

    front meio pronto

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react'
-
-
+import Header from './components/Header'
+import Footer from './components/Footer'
+import {Container} from 'react-bootstrap'
+import Feed from './screens/Feed'
+import ProductScreen from './screens/ProductScreen'
+import { BrowserRouter as Router, Route} from 'react-router-dom'
 const App = () => {
   return (
-    <div className="App">
-      daniel
-    </div>
+    <Router>
+    <Header />
+    <main className="py-3">
+      <Container>
+        <Route path="/" component={Feed} exact />
+        <Route path="/product/:id" component={ProductScreen} />
+      
+      </Container>
+
+    </main>
+    <Footer />  
+    </Router>
   );
 }
 
