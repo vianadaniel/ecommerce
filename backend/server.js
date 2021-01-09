d8f98a56678387652d6611bef58ec6a724d7cabf
Daniel Viana <vianadaniel@hotmail.com>
2021-01-09 14:44:14 -0300

    reducer

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from "express"
-import products from "./data/products.js"
+import productRouter from "./routes/productRouter.js"
 import dotenv from "dotenv"
 import connectDB from "./config/db.js"
 
@@ -13,14 +13,7 @@ app.get("/", (req, res) => {
   res.send("API is running")
 })
 
-app.get("/api/products", (req, res) => {
-  res.json(products)
-})
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((product) => product._id === req.params.id)
-  res.json(product)
-})
+app.use("/api/products", productRouter)
 
 const PORT = process.env.PORT || 5000
 
--- frontend/src/screens/Feed.js
+++ frontend/src/screens/Feed.js
@@ -1,30 +1,36 @@
-import React, { useState, useEffect } from "react"
+import React, { useEffect } from "react"
 import { Row, Col } from "react-bootstrap"
-
+import { listProducts } from "../actions/productActions"
+import { useDispatch, useSelector } from "react-redux"
 import Product from "../components/Product"
-import axios from "axios"
 
 const Feed = () => {
-  const [products, setProducts] = useState([])
+  const dispatch = useDispatch()
+
+  const productList = useSelector((state) => state.productList)
+
+  const { loading, error, products } = productList
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const res = await axios.get("/api/products")
-      setProducts(res.data)
-    }
-    fetchProducts()
-  }, [])
+    dispatch(listProducts())
+  }, [dispatch])
 
   return (
     <>
       <h1>Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h2>{error}</h2>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   )
 }
